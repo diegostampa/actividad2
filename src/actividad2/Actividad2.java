23aafec45137235337a6a1bd025eac63f24c1f92
RamiroMRomero <ramiromateoromero@sanluis.edu.ar>
2024-03-19 11:53:13 -0300

    Cambio yo
    Un cambio

--- src/actividad2/Actividad2.java
+++ src/actividad2/Actividad2.java
@@ -4,6 +4,8 @@ public class Actividad2 {
 
     public static void main(String[] args) {
         System.out.println("Con los muchachos");
+        
+        System.out.println("Esto es un cambio");
     }
     
 }
